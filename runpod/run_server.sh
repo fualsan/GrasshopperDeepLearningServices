acf542c97d128027e85b4b1d9cf89904ead0feed
Fuat Alsan <hfuatalsan@gmail.com>
2024-12-15 21:11:53 +0300

    runpod files are added

---
+++ runpod/run_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "INSTALLING THE APT DEPENDENCIES"
+apt-get update && apt-get install ffmpeg libsm6 libxext6 -y
+
+echo "INSTALLING PYTHON DEPENDENCIES"
+python -m pip install -U pip
+pip install fastapi uvicorn
+pip install -q diffusers transformers accelerate opencv-python controlnet-aux
+
+echo "SETTING UP THE HF CACHE"
+export HF_HOME="/workspace"
+export DISABLE_TELEMETRY="YES"
+
+echo "RUNNING THE SD SERVER"
+cd /workspace
+python sd_1_4_generic_server_runpod.py
+
+echo "ALL DONE"
